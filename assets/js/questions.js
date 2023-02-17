d7a7c5f524b1da3f0c6ed6ed71cd8b4aac07221a
Axel Jamal <axeltaj@gmail.com>
2023-02-17 17:44:35 +0000

    updated questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,38 +1,84 @@
+// Added list of questions, choices and answers
+
 const questions = [
   {
-    question: "Inside which HTML element do we put the JavaScript?",
-    choice: ["a. Scripting", "b. Javascript", "c. js", "d. script"],
-    answer: 0,
-  },
-  {
-    question: "How do you write 'Hello World' in an alert box?",
-    choice: [
-      "alert('Hello World')",
-      "alertBox('Hello World')",
-      "msg('Hello World')",
-      "msgBox('Hello World')",
+    title: "Where is the correct place to insert a JavaScript?",
+    choices: [
+      "The head section",
+      "Both the head section and the body section are correct",
+      "The body section",
+      "None of the above",
     ],
-    answer: "alert('Hello World')",
+    answer: "Both the head section and the body section are correct",
   },
   {
-    question: "How to write an IF statement in JavaScript?",
-    choice: ["if(i == 5)", "if i = 5 then", "if i == 5 then", "if i = 5"],
-    answer: "if(i == 5)",
+    title: "Inside which HTML element do we put the JavaScript?",
+    choices: ["<javascript>", "<src>", "<js>", "<script>"],
+    answer: "<script>",
   },
   {
-    question:
-      "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
-    choice: ["if(i != 5)", "if (i <> 5)", "if i =! 5 then", "if i <> 5"],
-    answer: "if(i != 5)",
-  },
-  {
-    question: "How can you add a comment in a JavaScript?",
-    choice: [
-      "//This is a comment",
-      "!--This is a commnet--",
-      "'This is a comment'",
-      "#This is a comment",
+    title:
+      "Is it necessary for the external script file to contain a <script> tag?",
+    choices: [
+      "Yes",
+      "Depends on the type of include",
+      "No",
+      "None of the above",
     ],
-    answer: "//This is a comment",
+    answer: "No",
+  },
+  {
+    title:
+      "What is the correct syntax for referring to an external script called 'gfg.js'?",
+    choices: [
+      "<script src='gfg.js'>",
+      "<script name='gfg.js'>",
+      "<script href='gfg.js'>",
+      "None of these",
+    ],
+    answer: "<script src='gfg.js'>",
+  },
+  {
+    title:
+      "How many ways are there with which we can declare a variable in javascript?",
+    choices: ["Only one", "Infinitely many", "Three", "None of the above"],
+    answer: "Three",
+  },
+  {
+    title: "Is a variable named 'apple' same as 'Apple' in javascript?",
+    choices: ["Only when we use 'strict'", "Yes", "No", "None of the above"],
+    answer: "No",
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["parentheses", "quotes", "curly brackets", "square brackets"],
+    answer: "parentheses",
+  },
+  {
+    title: "Arrays in JavaScript can be used to store ____.",
+    choices: [
+      "booleans",
+      "numbers and strings",
+      "other arrays",
+      "all of the above",
+    ],
+    answer: "all of the above",
+  },
+  {
+    title:
+      "String values must be enclosed within ____ when being assigned to variables.",
+    choices: ["quotes", "commas", "curly brackets", "parentheses"],
+    answer: "quotes",
+  },
+  {
+    title:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "console.log", "terminal / bash", "for loops"],
+    answer: "console.log",
+  },
+  {
+    title: "What is the type of Pop up boxes available in JavaScript?:",
+    choices: ["Alert", "Confirm", "Prompt", "All the above"],
+    answer: "All the above",
   },
 ];
